e9df0cea8022ebfbbd41456d87af19dd68556231
Yusuf0665 <95577518+Yusuf0665@users.noreply.github.com>
2022-12-06 03:01:02 +0300

    Add project files.

---
+++ ogrenci/Ogrenci.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogrenci
+{
+    public class Ogrenci
+    {
+        public ArrayList numara = new ArrayList();
+        public ArrayList ad = new ArrayList();
+        public ArrayList soyad = new ArrayList();
+        public ArrayList not = new ArrayList();
+
+        public void ogrenciEkle()
+        {
+            Console.WriteLine("Öğrenci numarasını giriniz: ");
+            int no = int.Parse(Console.ReadLine());
+            Console.WriteLine("Öğrencinin adını giriniz: ");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Öğrencinin soyadını giriniz: ");
+            string soyisim = Console.ReadLine();
+            Console.WriteLine("Öğrencinin notunu giriniz: ");
+            int puan = int.Parse(Console.ReadLine());
+
+            numara.Add(no);
+            ad.Add(isim);
+            soyad.Add(soyisim);
+            not.Add(puan);
+        }
+
+        public void ogrenciSil()
+        {
+            Console.WriteLine("Silmek istediğiniz öğrenci sırası:");
+
+            int ogrenciSirasi = int.Parse(Console.ReadLine());
+
+            numara.RemoveAt(ogrenciSirasi - 1);
+            ad.RemoveAt(ogrenciSirasi - 1);
+            soyad.RemoveAt(ogrenciSirasi - 1);
+            not.RemoveAt(ogrenciSirasi - 1);
+
+
+        }
+
+        public void ogrencileriListele()
+        {
+            for (int i = 0; i < numara.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". öğrencinin numarası: " + numara[i]);
+                Console.WriteLine((i + 1) + ". öğrencinin adı: " + ad[i]);
+                Console.WriteLine((i + 1) + ". öğrencinin soyadı: " + soyad[i]);
+                Console.WriteLine((i + 1) + ". öğrencinin puanı: " + not[i]);
+                Console.WriteLine("---------------------------------");
+            }
+        }
+
+        public void sayiGoster()
+        {
+            Console.WriteLine("Öğrenci Sayısı: " + numara.Count);
+        }
+
+        public void ortalamaHesapla()
+        {
+            int toplam = 0;
+            int ortalama;
+
+            for (int i = 0; i < not.Count; i++)
+            {
+                toplam += (int)not[i];
+            }
+            ortalama = toplam / not.Count;
+
+            Console.WriteLine("Öğrencilerin not ortalaması: " + ortalama);
+        }
+
+        public void cikis()
+        {
+            Console.WriteLine("İyi Günler :)");
+        }
+    }
+}
---
+++ ogrenci/Program.cs
@@ -0,0 +1,50 @@
+﻿using ogrenci;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Ogrenci ogr = new Ogrenci();
+
+        Console.WriteLine("Programa Hoş Geldiniz :)\n");
+        while (true)
+        {
+
+            Console.WriteLine("İşlemler");
+            Console.WriteLine("====================");
+            Console.WriteLine("1- öğrenci ekle");
+            Console.WriteLine("2- öğrenci sil");
+            Console.WriteLine("3- tüm öğrencileri listele");
+            Console.WriteLine("4- kayıtlı öğrenci sayısını göster");
+            Console.WriteLine("5- genel not ortalamasını hesapla");
+            Console.WriteLine("0- çıkış\n");
+            Console.WriteLine("Seçiminiz:");
+            int secim = int.Parse(Console.ReadLine());
+
+            switch (secim)
+            {
+                case 0: ogr.cikis(); break;
+                case 1: ogr.ogrenciEkle(); break;
+                case 2: ogr.ogrenciSil(); break;
+                case 3: ogr.ogrencileriListele(); break;
+                case 4: ogr.sayiGoster(); break;
+                case 5: ogr.ortalamaHesapla(); break;
+                default: Console.WriteLine("Geçerli seçim yapınız"); break;
+
+            }
+
+            if (secim == 0)
+            {
+                break;
+            }
+
+        }
+
+
+
+
+        Console.ReadKey();
+    }
+}
